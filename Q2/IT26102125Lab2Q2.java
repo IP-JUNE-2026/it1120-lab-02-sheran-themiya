281f1b4fcd1b79357b4220a38e722c40d76b4768
sheran-themiya <sheranthemiya15@gmail.com>
2026-06-17 18:49:35 +0530

    IT26102125Lab2Q2.java

---
+++ Q2/IT26102125Lab2Q2.java
@@ -0,0 +1,8 @@
+public class IT26102125Lab2Q2 {
+  public static void main (String [] args) {
+	double length = 10.0;
+	double perimeter_of_a_square = length*4;
+    double radius = perimeter_of_a_square/(2*Math.PI);
+    System.out.println ("Radius of the circular fence: " +radius);
+  }
+}
