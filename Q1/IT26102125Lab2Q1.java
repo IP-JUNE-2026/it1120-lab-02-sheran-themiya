f40280f09a84dbdaef297853d6c09727478061e5
sheran-themiya <sheranthemiya15@gmail.com>
2026-06-17 18:48:40 +0530

    IT26102125Lab2Q1.java

---
+++ Q1/IT26102125Lab2Q1.java
@@ -0,0 +1,11 @@
+public class IT26102125Lab2Q1 {
+  public static void main (String [] args) {
+	  
+	  double perimeter = 100.0 ;
+	  double length = 200/7 ;
+	  double width = (3.00/4.00)*length ;
+	  
+      System.out.println ("Lenght of the fence :" +length);
+	  System.out.println ("Width of the fence : " +width) ;
+  }
+}
