a71b17e52cfd52aae4553dba59d3e7241480c00f
sheran-themiya <sheranthemiya15@gmail.com>
2026-06-21 09:08:55 +0530

    IT26102125Lab2Q3.java

---
+++ Q3/IT26102125Lab2Q3.java
@@ -0,0 +1,10 @@
+public class IT26102125Lab2Q3 {
+    public static void main(String[] args) {
+		
+        double sideA = 3.0;
+        double sideB = 4.0;
+        double hypotenuse = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+		
+        System.out.println("Length of the hypotenuse: " + hypotenuse);
+    }
+}
